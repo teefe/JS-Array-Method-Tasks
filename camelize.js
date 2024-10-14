d6828fb84c7c32fd1a67db8ac565f980886a7849
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2024-10-14 04:20:30 +0300

    first dash error issue fixed

--- camelize.js
+++ camelize.js
@@ -1,5 +1,8 @@
 function camelize(stmnt) {
     let splitArry = stmnt.split("-");
+    if (splitArry[0] == '') {
+        splitArry.shift();
+    }
     const camelArry = splitArry.map(row => [row[0].toUpperCase(), ...row.slice(1)]);
     let joinedArry = camelArry.join("");
     return joinedArry.split(",").join("");
